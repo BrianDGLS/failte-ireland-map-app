45fe2b3ae0ac5a0bc190556b7943c812233aae10
BrianDGLS <briandgls92@gmail.com>
2020-06-02 16:21:09 +0100

    feat: added basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfoPanelComponent } from './info-panel/info-panel.component';
+import { MaterialModule } from './material-module';
+import { HeaderComponent } from './header/header.component';
+import { MapPanelComponent } from './map-panel/map-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoPanelComponent,
+    HeaderComponent,
+    MapPanelComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
