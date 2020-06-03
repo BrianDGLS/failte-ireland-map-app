8af4e84f2147cd47e53bef359124bf20c6449d5f
BrianDGLS <briandgls92@gmail.com>
2020-06-03 12:28:35 +0100

    feat: rendering basic icons

--- src/app/map-panel/map-panel.component.ts
+++ src/app/map-panel/map-panel.component.ts
@@ -6,10 +6,13 @@ import {
   defaults as defaultInteractions,
   DragRotateAndZoom,
 } from 'ol/interaction';
-import TileLayer from 'ol/layer/Tile';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
-
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Icon, Style } from 'ol/style';
+import VectorSource from 'ol/source/Vector';
 import { DataService } from '../data.service';
 
 @Component({
@@ -24,13 +27,36 @@ export class MapPanelComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     const data = await this.dataService.getAttractionsJson();
-    var map = new Map({
-      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
-      layers: [
-        new TileLayer({
-          source: new OSM(),
+
+    const subset = data
+      .filter((_) => _.Url && _.Longitude && _.Latitude)
+      .slice(0, 20);
+    const features = [];
+
+    for (const sub of subset) {
+      const feature = new Feature({
+        name: sub.Name,
+        geometry: new Point(fromLonLat([sub.Longitude, sub.Latitude])),
+      });
+      var iconStyle = new Style({
+        image: new Icon({
+          anchor: [0.5, 46],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: 'https://openlayers.org/en/v4.6.5/examples/data/icon.png',
         }),
-      ],
+      });
+
+      feature.setStyle(iconStyle);
+      features.push(feature);
+    }
+
+    const source = new VectorSource({ features });
+    const vectorLayer = new VectorLayer({ source });
+
+    const map = new Map({
+      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+      layers: [new TileLayer({ source: new OSM() }), vectorLayer],
       target: 'map',
       view: new View({
         center: fromLonLat(this.center),
